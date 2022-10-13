6c955255e6dcabf98c285ffe12cb825862ee3ecc
Patricio Negrete <pnegrete@mma.gob.cl>
2022-10-13 11:47:29 -0300

    cambio

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com',
+    domains: ['res.cloudinary.com'],
+  }
 }
 
 module.exports = nextConfig
